92bca70b5584c220618f67e78c6bb56bdc48ebe8
Henry Poulton <hgp10@ic.ac.uk>
2014-02-18 22:33:47 +0000

    Up to optimising memory accesses

--- test/timeoutputs.ps1
+++ test/timeoutputs.ps1
@@ -1,15 +1,19 @@
 cd C:\Users\Henry\Dropbox\HPC\Coursework\CW4\hpce-2013-cw4\bin
 
+.\make_world 1024 > world.txt
+
 $env:HPCE_CL_SRC_DIR="../src/hgp10"
 
-Measure-Command {.\make_world 100 0.1 | .\step_world 0.1 10000 > $null}
-Measure-Command {.\make_world 100 0.1 | .\step_world_v1_lambda 0.1 10000 > $null}
-Measure-Command {.\make_world 100 0.1 | .\step_world_v2_function 0.1 10000 > $null}
+# make_world dimensions propagation_constant
+# step_world time_step number_of_steps
+Measure-Command {cat .\world.txt | .\step_world 0.1 1000 > $null}
+Measure-Command {cat .\world.txt | .\step_world_v1_lambda 0.1 1000 > $null}
+Measure-Command {cat .\world.txt | .\step_world_v2_function 0.1 1000 > $null}
 
 $env:HPCE_SELECT_DEVICE="0"
-Measure-Command {.\make_world 100 0.1 | .\step_world_v3_opencl 0.1 10000 > $null}
+Measure-Command {cat .\world.txt | .\step_world_v3_opencl 0.1 0 > $null}
 
 $env:HPCE_SELECT_DEVICE="1"
-Measure-Command {.\make_world 100 0.1 | .\step_world_v3_opencl 0.1 10000 > $null}
+Measure-Command {cat .\world.txt | .\step_world_v3_opencl 0.1 1000 > $null}
 
 pause
